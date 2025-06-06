ed198de25ecfe4d8bf19466f0e5784abf1cd6589
loks0n <22452787+loks0n@users.noreply.github.com>
2025-06-06 16:44:10 +0100

    chore: orbstack

--- config.fish
+++ config.fish
@@ -11,3 +11,7 @@ fish_add_path "/Users/loks0n/.local/bin"
 # aliases
 alias lg="lazygit log"
 alias k="kubectl"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
