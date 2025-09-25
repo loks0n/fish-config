15aca5a568b5dba050d16dba2d7014c41418f719
loks0n <22452787+loks0n@users.noreply.github.com>
2025-09-25 16:16:56 +0100

    feat: git shortcuts

--- config.fish
+++ config.fish
@@ -12,6 +12,25 @@ fish_add_path "/Users/loks0n/.local/bin"
 alias lg="lazygit log"
 alias k="kubectl"
 
+# git
+alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
+
+alias ga="git add"
+alias gap="git add --patch"
+alias gc="git commit"
+
+alias gs="git switch"
+alias gr="git restore"
+
+alias gp="git push"
+alias gu="git pull"
+
+alias gl='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(auto)  %D%n%s%n"'
+alias gb="git branch"
+
+alias gi="git init"
+alias gcl="git clone"
+
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
